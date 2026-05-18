6bf0e98d561f199c097925d359ecafdfa55c7411
xenutrx <2977762731@qq.com>
2026-05-18 11:23:09 +0800

    Update meowfficer_farming.py 短猫处进入71练级锁定第3舰队

--- module/os/tasks/meowfficer_farming.py
+++ module/os/tasks/meowfficer_farming.py
@@ -141,7 +141,7 @@ class OpsiMeowfficerFarming(OSMap):
                 logger.hr(f'OS hazard 1 leveling, zone_id={zone}', level=1)
                 if self.zone.zone_id != zone or not self.is_zone_name_hidden:
                     self.globe_goto(self.name_to_zone(zone), types='SAFE', refresh=True)
-                self.fleet_set(self.config.OpsiFleet_Fleet)
+                self.fleet_set(3)
                 self.run_strategic_search()
 
                 self.handle_after_auto_search()
